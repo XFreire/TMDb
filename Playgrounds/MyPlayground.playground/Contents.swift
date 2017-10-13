7f5fc47972aecb22890f2cf49323827891925377
Alexandre Freire García <xandrefreire@gmail.com>
2017-10-13 16:26:11 +0200

    Add minimum playground support

---
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+//: Playground - noun: a place where people can play
+
+import TMDbCore
+import RxSwift
+
+var str = "Hello, playground"
---
+++ TMDbCore/TMDbCore.swift
@@ -0,0 +1,9 @@
+//
+//  TMDbCore.swift
+//  TMDbCore
+//
+//  Created by Alexandre Freire García on 13/10/17.
+//  Copyright © 2017 Alexandre Freire García. All rights reserved.
+//
+
+import Foundation
