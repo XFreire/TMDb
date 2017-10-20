71b05a244e967442eebaf25f556b87d0a3fa7ac6
Alexandre Freire García <xandrefreire@gmail.com>
2017-10-20 13:21:23 +0100

    Add detail about view

---
+++ TMDbCore/Details/Presentation/DetailAboutView.swift
@@ -0,0 +1,16 @@
+//
+//  DetailAboutView.swift
+//  TMDbCore
+//
+//  Created by Alexandre Freire García on 20/10/17.
+//  Copyright © 2017 Alexandre Freire García. All rights reserved.
+//
+
+import Foundation
+
+final class DetailAboutView: UIView, NibLoadableView {
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+}
