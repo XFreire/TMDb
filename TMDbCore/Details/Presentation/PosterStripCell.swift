3a9faed224d96ccceffbafbc8b116fa683b3f2f8
Alexandre Freire García <xandrefreire@gmail.com>
2017-10-20 15:28:45 +0100

    Add poster strip cell

---
+++ TMDbCore/Details/Presentation/PosterStripCell.swift
@@ -0,0 +1,29 @@
+//
+//  PosterStripCell.swift
+//  TMDbCore
+//
+//  Created by Alexandre Freire García on 20/10/17.
+//  Copyright © 2017 Alexandre Freire García. All rights reserved.
+//
+
+import RxSwift
+
+class PosterStripCell: UICollectionViewCell {
+
+    // MARK: - Outlets
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var metadataLabel: UILabel!
+    
+    // MARK: - Properties
+    private(set) var disposeBag = DisposeBag()
+    
+    // MARK: - Overrides
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        posterView.image = nil
+        disposeBag = DisposeBag()
+    }
+
+}
