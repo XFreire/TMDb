80b8d9debf4745dc31d560d40c48c24845859638
Alexandre Freire García <xandrefreire@gmail.com>
2017-10-23 16:16:44 +0100

    Add ShowDetail model

--- TMDbCore/Details/Domain/ShowDetail.swift
+++ TMDbCore/Details/Domain/ShowDetail.swift
@@ -1,32 +1,29 @@
 //
-//  MovieDetail.swift
+//  ShowDetail.swift
 //  TMDbCore
 //
-//  Created by Alexandre Freire García on 21/10/17.
+//  Created by Alexandre Freire García on 23/10/17.
 //  Copyright © 2017 Alexandre Freire García. All rights reserved.
 //
 
 import Foundation
 
-struct MovieDetail: Decodable {
+struct ShowDetail: Decodable {
     let backdropPath: String?
     let identifier: Int64
     let overview: String?
     let posterPath: String?
-    let releaseDate: String?
-    let runtime: Int
+    let firstAirDate: String?
     let title: String
     let credits: Credits?
-
+    
     private enum CodingKeys: String, CodingKey {
         case backdropPath = "backdrop_path"
         case identifier = "id"
         case overview
         case posterPath = "poster_path"
-        case releaseDate = "release_date"
-        case runtime
-        case title
+        case firstAirDate = "first_air_date"
+        case title = "name"
         case credits
     }
 }
-
