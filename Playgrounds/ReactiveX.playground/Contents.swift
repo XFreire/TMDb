475a24de896747a1b1cbc16011af5bcc304b04a5
Alexandre Freire García <xandrefreire@gmail.com>
2017-11-15 11:59:30 +0100

    ReactiveX playground - Step 1.2: Observable.create

--- Playgrounds/ReactiveX.playground/Contents.swift
+++ Playgrounds/ReactiveX.playground/Contents.swift
@@ -13,3 +13,10 @@ let empty = Observable<Int>.empty() // Completed only
 let single = Observable.just("Hello") // Next and completed
 let some = Observable.from(["🤔", "😎", "💩"]) // How many events are sent?: 4
 let error = Observable<Data>.error(APIError.invalidKey)
+
+let hello = Observable<String>.create { observer in // observer = future subscriber
+    observer.onNext("Hello")
+    observer.onCompleted()
+    
+    return Disposables.create()
+}
